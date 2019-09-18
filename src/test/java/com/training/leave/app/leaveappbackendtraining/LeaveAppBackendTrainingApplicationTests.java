83cb6c26488d651c429a0658c37a8685ec16a369
AF67603 <abishek.sahai@anthem.com>
2019-09-17 20:19:17 -0400

    added test case

--- src/test/java/com/training/leave/app/leaveappbackendtraining/LeaveAppBackendTrainingApplicationTests.java
+++ src/test/java/com/training/leave/app/leaveappbackendtraining/LeaveAppBackendTrainingApplicationTests.java
@@ -1,16 +1,18 @@
-//package com.training.leave.app.leaveappbackendtraining;
-//
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//public class LeaveAppBackendTrainingApplicationTests {
-//
-//	@Test
-//	public void contextLoads() {
-//	}
-//
-//}
+package com.training.leave.app.leaveappbackendtraining;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.training.leave.app.component.LeaveAppBackendTrainingApplication;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes=LeaveAppBackendTrainingApplication.class)
+public class LeaveAppBackendTrainingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
